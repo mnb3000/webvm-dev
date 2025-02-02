226852ec85345d73be9b0b6be7c848b6c5b96297
mnb3000 <misha.mykolyshyn@gmail.com>
2025-02-03 00:24:27 +0200

    chore: update terminal config

--- config_github_terminal.js
+++ config_github_terminal.js
@@ -3,21 +3,17 @@ export const diskImageUrl = IMAGE_URL;
 // The root filesystem backend type
 export const diskImageType = "github";
 // Print an introduction message about the technology
-export const printIntro = true;
+export const printIntro = false;
 // Is a graphical display needed
 export const needsDisplay = false;
 // Executable full path (Required)
-export const cmd = CMD; // Default: "/bin/bash";
+export const cmd = "/bin/zsh";
 // Arguments, as an array (Required)
-export const args = ARGS; // Default: ["--login"];
+export const args = [];
 // Optional extra parameters
 export const opts = {
-	// Environment variables
-	env: ENV, // Default: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
-	// Current working directory
-	cwd: CWD, // Default: "/home/user",
-	// User id
-	uid: 1000,
-	// Group id
-	gid: 1000
+  // User id
+  uid: 0,
+  // Group id
+  gid: 0,
 };
